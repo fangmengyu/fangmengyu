ffeb94acb4848a23db6bda74765d5d88d3a906eb
mianfengchen <457668569@qq.com>
2017-09-05 18:20:54 +0800

    express配置

---
+++ public/js/login.js
@@ -0,0 +1,15 @@
+
+window.addEventListener('load',function(){
+
+    $('#login').addEventListener('submit',function(e){
+        e.preventDefault();
+
+        var action = this.action;
+        var data   = Qs_Form.getData(this);
+
+
+    })
+
+})
+
+
