76cdc49ad0460519644a05f03e8d46d063346388
mianfengchen <457668569@qq.com>
2017-09-14 14:10:57 +0800

    后台配置

--- public/js/base.js
+++ public/js/base.js
@@ -2,72 +2,26 @@ const $  = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 
-function getFromData(el){
-    var inps = $$.bind(el)('input[name],textarea[name],select[name]');
-    var res  = {};
-    inp.foreach( (n,i,a)=>{
-        if  (n.type == 'radio') {
-            n.checked && res.push(n.name,n.value);
-        } else if (n.type == 'checkbox'){
-            if ( n.checked ) {
-                res[n.name] ? res[n.name].push(n.value) : res.push(n.name,[n.value]);
-            }
-        } else {
-            res.push(n.name,n.value);
-        }
-    } )
-    return res;
-
-}
-
-var Qs = function(){
-
-    var getFromData = function(el){
-        var inps = $$.bind(el)('input[name],textarea[name],select[name]');
-        var res  = {};
-        inp.foreach( (n,i,a)=>{
-            if  (n.type == 'radio') {
-                n.checked && res.push(n.name,n.value);
-            } else if (n.type == 'checkbox'){
-                if ( n.checked ) {
-                    res[n.name] ? res[n.name].push(n.value) : res.push(n.name,[n.value]);
+var Qs = (function(){
+    return {
+        getFromData : function(el){
+            var inps = $$.bind(el)('input[name],textarea[name],select[name]');
+            var res  = {};
+            inps.foreach( (n,i,a) => {
+                if  (n.type == 'radio') {
+                    n.checked && (res[n.name] = n.value);
+                } else if (n.type == 'checkbox'){
+                    if ( n.checked ) {
+                        res[n.name] ? res[n.name].push(n.value) : res[n.name] = n.value;
+                    }
+                } else {
+                    res[n.name] = n.value;
                 }
-            } else {
-                res.push(n.name,n.value);
-            }
-        } )
-        return res;
-    };
-
-
-    //  导出 API
-
-    this.getFromData = getFromData;
-
-
-};
-
-var qs = new Qs();
-
-console.log(qs.getFromData)
-
-
-$('input.phone').on('keyup',function(){
-    this.value = this.value.replace(/[^\d]/g,'');
-})
-
-$('input.phone').on('blur',function(){
-    if ( /^1[3-9]\d{9}$/.test(this.value) ) {
-        // 如果输入的是正确的手机号
-    } else {
-        // 如果输入手机号不合法
+            } )
+            return res;
+        }
     }
-})
 
-function checkPhone(this,fn1,fn2){
-    if ( /^1[3-9]\d{9}$/.test(this.value) ) {
-        fn1(this);
-    } else {
-        fn2(this);
-    }
-}
+})();
+
+console.log(Qs.getFromData)
--- public/js/login.js
+++ public/js/login.js
@@ -3,13 +3,33 @@ window.addEventListener('load',function(){
 
     $('#login').addEventListener('submit',function(e){
         e.preventDefault();
-
+        console.log(1);
         var action = this.action;
-        var data   = Qs_Form.getData(this);
+        // var data   = Qs_Form.getData(this);
 
 
     })
+    
+    $('.ceshi').addEventListener('click',AD.add,false);
 
 })
+
+
+var AD = {
+    a : 1,
+    add : function(){
+        console.log(AD.a)
+        console.log(this.a);
+    }
+}
+
+var AD = (function () {
+    var _s = {
+
+    };
+    return _s;
+
+})();
+
 
 
